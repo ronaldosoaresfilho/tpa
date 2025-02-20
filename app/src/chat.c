fe5ae8784cf9b5a595ba32d353f1e5760f46ceb2
ron <ron@github.users.noreply>
2025-02-20 17:17:00 -0300

    removidos os módulos em python, fazendo tudo em C a partir de agora

--- app/src/chat.c
+++ app/src/chat.c
@@ -1,69 +1,12 @@
 #include "common.h"
 #include "chat.h"
 
-// Função de conversa
 char *chat(const char *text)
 {
-    const char *input_file = "input.txt";
-    const char *output_file = "output.txt";
-    const char *chat_script = "app/src/chat.py";
-    FILE *fp;
-
-    const char *input_data = (char*) text;
-
-    fp = fopen(input_file, "w");
-
-    fprintf(fp, "%s\n", input_data);
-    fclose(fp);
-
-    char command[256];
-    snprintf(command, sizeof(command), "python %s", chat_script);
-    system(command);
-
-    fp = fopen(output_file, "r");
-
-    char output[256];
-    if (fgets(output, sizeof(output), fp) != NULL) {
-        //printf("Saída do Python: %s\n", output);
-    }else {
-        perror("Erro ao ler a saída do arquivo");
-    }
-    fclose(fp);
-
-    remove(input_file);
-    remove(output_file);
-
-    char *t = (char*) malloc(LINESIZE * sizeof(char));
-    sprintf(t, "%s", output);
-
-    printf("user: %s\n", text);
-    printf("chat: %s\n", t);
-
-    return t;
+    return text;
 }
 
 char *train(void)
 {
-    char command[256];
-    const char *train_script = "app/src/train.py";
- 
-    snprintf(command, sizeof(command), "python %s", train_script);
-    system(command);
-
-    // se há o arquivo train_end.txt e se ele contem "ok" 
-    FILE *fp;
-    char *train_end = "train_end.txt";
-    char *t = (char*) malloc(LINESIZE * sizeof(char));
-
-    fp = fopen(train_end, "r");
-
-    if (fp != NULL) {   
-        sprintf(t, "%s", "Treino finalizado!");
-        remove(train_end);
-        fclose(fp);
-    }else {
-        sprintf(t, "%s", "Não houve treino!");
-    }
-    
-    return t;
+    return "ok";
 }
